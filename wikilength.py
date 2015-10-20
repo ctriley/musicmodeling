5bf852a53150022a41afd37ddee0cebb21fe67a8
Connor Riley <connor.riley@uconn.edu>
2015-10-19 20:04:10 -0400

    able to parse 1 page

--- wikilength.py
+++ wikilength.py
@@ -1,1 +1,13 @@
 ## get song length from wikipedia.
+import wikipedia
+import re
+
+
+expr = re.compile('class="duration"><span class="min">(\d)*</span>:<span class="s">(\d)+</span></span>')
+test = wikipedia.page("King Kunta")
+s = test.html()
+span = expr.search(s).span()
+(x,y) = span
+sub = s[x:y]
+
+
