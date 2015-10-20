3569e69824a7d61a4458f40cf7c319f40c55c6c0
Connor Riley <connor.riley@uconn.edu>
2015-10-19 20:31:41 -0400

    added input csv

--- wikilength.py
+++ wikilength.py
@@ -1,13 +1,31 @@
 ## get song length from wikipedia.
-import wikipedia
 import re
+import csv
+import wikipedia
 
+# import csv file (Data)
 
-expr = re.compile('class="duration"><span class="min">(\d)*</span>:<span class="s">(\d)+</span></span>')
-test = wikipedia.page("King Kunta")
-s = test.html()
-span = expr.search(s).span()
-(x,y) = span
-sub = s[x:y]
-
-
+with open('data.csv', 'rb') as f:
+	reader = csv.reader(f)
+	songlist = list(reader)
+# regular expression needs generalizing for other songs
+# need to output result into csv
+expr = re.compile('class="duration"><span class="min">(\d)*</span>:<span class="s">(\d)+')
+for line in songlist:
+	#pagetitle = wikipedia.suggest(line[1] + " music")
+	#if(pagetitle != None):
+	try:
+		test = wikipedia.page("Hotline Bling") # replace "hotline bling" with line[1]
+		s = test.html()
+		search = expr.search(s)
+		print search
+		if(search != None):
+			span = search.span()
+			(x,y) = span
+			sub = s[x:y]
+			print sub
+	except wikipedia.exceptions.PageError:
+		print "page not found"
+			# do nothing
+	except wikipedia.exceptions.DisambiguationError:
+		print "too many results"
