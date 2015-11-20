8ed273fb472fb81b58c02b43832c4d77624f810d
Connor Riley <connor.riley@uconn.edu>
2015-11-20 12:04:22 -0500

    script gets all lengths

--- byyear.py
+++ byyear.py
@@ -11,7 +11,6 @@ import urllib
 def getURLList(startURL):
 	context = ssl._create_unverified_context()
 	website = urllib.urlopen(startURL, context=context)
-	print startURL
 	#website = urllib2.urlopen(startURL)
 	#read html code
 	html = website.read()
@@ -21,12 +20,36 @@ def getURLList(startURL):
 	#links = re.findall('"((http|ftp)s?://.*?)"', html)
 	return links
 
+def getTableURLS(beginpage):
+	wikipage = wikipedia.page(beginpage)
+	starturl = wikipage.url
+	links = getURLList(starturl)
+	actuallinks = []
+	linkprefix = "https://www.wikipedia.org"
+	for link in links:
+		actuallink = linkprefix + link[14:-7]
+		actuallinks.append(actuallink)
+	return actuallinks
+
+def getSongLengths(links):
+	context = ssl._create_unverified_context()
+	songdictionary = {}
+	for link in links:
+		website = urllib.urlopen(link, context=context)
+		html = website.read()
+		a = re.findall(lenghtexpr, html)
+		if(len(a) > 0):
+			songlength = re.findall(timeexpr, a[0])
+			songdictionary[link] = songlength[0]
+			print songdictionary[link]
+	print len(songdictionary)
+	return songdictionary
 with open('data.csv', 'rb') as f:
 	reader = csv.reader(f)
 	songlist = list(reader)
 
-expr = re.compile('class="min">(\d)*</span>:<span class="s">(\d)+')
-
+lenghtexpr = re.compile('<th scope="row">Length</th>\n<td>[<b>]*\d*:\d\d')
+timeexpr = re.compile('\d*:\d\d')
 year = "1980"
 beginpage = "Billboard Year-End Hot 100 singles of " + str(year)
 yearmap = {}
@@ -35,12 +58,11 @@ for i in range(1980, 2010):
 	yearmap[str(i)] = j
 	j = j + 100
 
-wikipage = wikipedia.page(beginpage)
-starturl = wikipage.url
-links = getURLList(starturl)
-print links
 startRange = yearmap[year]
 endRange = startRange + 100
+links = getTableURLS(beginpage)
+songdictionary = getSongLengths(links)
+
 
 for i in range(startRange, endRange):
 	songname = songlist[i][1]
