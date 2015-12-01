4de5683d18d7ca7703d570cf42ecfd035a9cde85
Connor Riley <connor.riley@uconn.edu>
2015-12-01 13:06:14 -0500

    completed a leastsquares training script

--- leastsquares.py
+++ leastsquares.py
@@ -1,4 +1,8 @@
 import csv
+import datetime
+import time
+from sklearn import linear_model # pip install -U scikit-learn
+
 
 def gettitlelength(title):
 	if '(' in title:
@@ -6,6 +10,19 @@ def gettitlelength(title):
 		title = title[0]
 	return len(title)
 
+def mintosec(songtime):
+	while(len(songtime)) < 8:
+		if len(songtime) == 2 or len(songtime) == 5:
+			songtime = ':' + songtime
+		else: 
+			songtime = '0' + songtime # 00003:45
+	try:
+		x = time.strptime(songtime,'%H:%M:%S')
+		totalseconds = datetime.timedelta(hours=x.tm_hour,minutes=x.tm_min,
+			seconds=x.tm_sec).total_seconds()
+		return totalseconds
+	except:
+		return None
 
 def readdata():
 	x = []
@@ -17,17 +34,23 @@ def readdata():
 				rank = row[0]
 				title = row[1]
 				length = row[4]
-				titlelength = gettitlelength(title)
-				y.append(rank)
-				x.append([titlelength, length])
+				length = mintosec(length)
+				if length != None:
+					titlelength = gettitlelength(title)
+					y.append(int(rank))
+					titlelength = int(titlelength)
+					length = int(length)
+					x.append([titlelength, length])
 		return (x,y)
 
-
+def leastsquares(x,y):
+	clf = linear_model.LinearRegression()
+	clf.fit(x,y)
+	print('Coefficients: \n', clf.coef_)
 
 def main():
 	(x,y) = readdata()
-	print len(y)
-	print len(x)
+	leastsquares(x,y)
 
 if __name__ == "__main__": 
 	main()
