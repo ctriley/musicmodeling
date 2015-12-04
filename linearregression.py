9057351a95b455549be7536942993a793c406afa
Connor Riley <connor.riley@uconn.edu>
2015-12-03 21:32:23 -0500

    added evaluation methods

--- linearregression.py
+++ linearregression.py
@@ -43,26 +43,56 @@ def readdata():
 					x.append([titlelength, length])
 		return (x,y)
 
-def leastsquares(x,y):
+def readdata2014():
+	x = []
+	y = []
+	with open('out.csv', 'r') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			if int(row[3]) == 2014:
+				rank = row[0]
+				title = row[1]
+				length = row[4]
+				length = mintosec(length)
+				if length != None:
+					titlelength = gettitlelength(title)
+					y.append(int(rank))
+					titlelength = int(titlelength)
+					length = int(length)
+					x.append([titlelength, length])
+		return (x,y)
+
+
+def rsquared(regressiontype, ypredict, y1):
+	print regressiontype
+	sklearn.metrics.r2_score(y1, ypredict)
+	
+
+def leastsquares(x,y, x1, y1):
 	clf = linear_model.LinearRegression()
 	clf.fit(x,y)
-	print('Coefficients: \n', clf.coef_)
+	ypredict = clf.predict(x1)
+	rsquared("least squares",ypredict, y1)
 
-def ridgeregression(x,y):
+def ridgeregression(x,y,x1,y1):
 	clf = linear_model.Ridge(alphas = [.1,.5,1,10])
 	clf.fit(x,y)
-	print clf.alpha_
+	ypredict = clf.predict(x1)
+	rsquared("ridgeregression",ypredict, y1)
 
-def lasso(x,y):
+def lasso(x,y,x1,y1):
 	clf = linear_model.Lasso(alphas = [.1,.5,1,10])
 	clf.fit(x,y)
-	print clf.alpha_
-
-
+	ypredict = clf.predict(x1)
+	rsquared("lasso",ypredict, y1)
 
 def main():
 	(x,y) = readdata()
-	leastsquares(x,y)
+	(x1, y1) = readdata2014()
+	leastsquares(x,y,x1,y1)
+	ridgeregression(x,y,x1,y1)
+	lasso(x,y,x1,y1)
+
 
 if __name__ == "__main__": 
 	main()
